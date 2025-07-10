cc8bae8f2d216a1d2c299508ebbb095d15aff51d
pedro oliveira <pvmgo123@gmail.com>
2025-07-10 11:14:52 -0300

    feat: add Bank class and initial project structure

---
+++ scr/app.py
@@ -0,0 +1,13 @@
+class Bank():
+    def __init__(self):
+        self.balance = 0
+        self.extract = []
+    
+    def withdraw(self, value):
+        pass
+
+    def deposit(self, value):
+        pass
+
+    def view_extract(self):
+        pass
