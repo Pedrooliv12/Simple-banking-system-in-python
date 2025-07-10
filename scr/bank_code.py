d9970b1d955d7a46a06a621d36a92695d1025f94
pedro oliveira <pvmgo123@gmail.com>
2025-07-10 19:10:24 -0300

    feat: implement extract method in Bank class

---
+++ scr/bank_code.py
@@ -0,0 +1,37 @@
+class Bank():
+    def __init__(self):
+        self.balance = 0
+        self.extract = {"withdraw": [], "deposit": []}
+    
+
+    def withdraw(self, amount):
+        
+        if (amount <= 0):
+            return print("Incorrect value")
+            
+        elif (amount > self.balance):
+            return print("Amount reported above the balance")
+        
+        elif (amount > 500):
+            print("Maximum withdrawal limit of R$500")
+        
+        else:
+            self.balance -= amount
+            self.extract["withdraw"].append(amount)
+
+
+    def deposit(self, amount):
+        
+        if (amount <= 0):
+            print("Incorrect value")
+
+        else:
+            self.balance += amount
+            self.extract["deposit"].append(amount)
+
+
+    def view_extract(self):
+        for type, values in self.extract.items():
+            for value in values:
+                print(f"{type}: R${value:.2f}")
+        print(f"Saldo atual: R${self.balance:.2f}")
