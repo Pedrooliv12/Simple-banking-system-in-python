75199d42e5a3441f22b992632bdc9741c59c4334
pedro oliveira <pvmgo123@gmail.com>
2025-07-10 11:31:33 -0300

    feat: implement withdraw method in Bank class

--- scr/app.py
+++ scr/app.py
@@ -3,11 +3,23 @@ class Bank():
         self.balance = 0
         self.extract = []
     
-    def withdraw(self, value):
+
+    def withdraw(self, amount):
+        
+        if (amount <= 0):
+            return print("Incorrect value")
+            
+        elif (amount > self.balance):
+            return print("Amount reported above the balance")
+        
+        else:
+            self.balance -= amount
+            self.extract.append(amount)
+
+
+    def deposit(self, amount):
         pass
 
-    def deposit(self, value):
-        pass
 
     def view_extract(self):
         pass
